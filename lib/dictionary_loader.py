069dec06074828c9f0683847a9ca71329293831d
Samuel Ballan <sgb4622@gmail.com>
2022-03-18 14:48:17 -0400

    dictionary class

--- lib/dictionary_loader.py
+++ lib/dictionary_loader.py
@@ -1,27 +1,24 @@
 import os
 script_dir = os.path.dirname(__file__)
 
-class DictionaryLoader:
-    def __init__(self):
-        self.__dictionary1_string = ""
-        self.__dictionary2_string = ""
+class Dictionary:
+    def __init__(self, num):
+        if num not in [1, 2]:
+            raise(BaseException("Invalid dictionary number!"))
+
+        self.__num = num
+        self.__string = ""
 
         current_dir = os.path.dirname(__file__)
 
-        with open(os.path.join(current_dir, "dictionaries/dictionary_1.txt")) as f:
-            self.__dictionary1_string = f.read()
+        with open(os.path.join(current_dir, f"dictionaries/dictionary_{num}.txt")) as f:
+            self.__string = f.read()
 
-        with open(os.path.join(current_dir, "dictionaries/dictionary_2.txt")) as f:
-            self.__dictionary2_string = f.read()
+    def string(self):
+        return self.__string
 
-    def dictionary1_string(self):
-        return self.__dictionary1_string
-
-    def dictionary2_string(self):
-        return self.__dictionary2_string
-
-    def dictionary1(self):
-        return self.__dictionary1_string.split("\n")
-
-    def dictionary2(self):
-        return self.__dictionary2_string.split("\n")
+    def words(self):
+        if self.__num == 1:
+            return self.__string.split("\n")
+        else:
+            return self.__string.split(" ")
